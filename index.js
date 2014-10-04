c937b75fe1c67caef9a70d040a9ce74adecc88d7
Shawn French <shawn.french@jadedpixel.com>
2014-10-04 16:36:28 +0000

    Added more events to support ecommerce

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ module.exports = {
   viewedProduct: /^[ _]?viewed[ _]?product[ _]?$/i,
   viewedProductCategory: /^[ _]?viewed[ _]?product[ _]?category[ _]?$/i,
   addedProduct: /^[ _]?added[ _]?product[ _]?$/i,
-  completedOrder: /^[ _]?completed[ _]?order[ _]?$/i
+  completedOrder: /^[ _]?completed[ _]?order[ _]?$/i,
+  refundedOrder: /^[ _]?refunded?[ _]?order[ _]?$/i,
+  viewedProductDetails: /^[ _]?viewed[ _]?product[ _]?detail[s]?[ _]?$/i,
+  clickedProduct: /^[ _]?clicked[ _]?product[ _]?$/i,
+  viewedPromotion: /^[ _]?viewed[ _]?promo(tion)?[ _]?$/i,
+  clickedPromotion: /^[ _]?clicked[ _]?promo(tion)?[ _]?$/i,
+  startedOrder: /^[ _]?started[ _]?order[ _]?$/i,
+  updatedOrder: /^[ _]?updated[ _]?order[ _]?$/i,
+  configuredOrder: /^[ _]?configured[ _]?order[ _]?$/i,
 };
