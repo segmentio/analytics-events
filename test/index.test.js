d13f795ac84675bbf743d4826a2a4385d2ba8442
wcjohnson11 <williamcouperjohnson@gmail.com>
2016-06-03 20:50:50 +0000

    remove unused deps

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,10 @@
 'use strict';
 
-var to = require('to-case');
+var sentence = require('to-sentence-case');
+var capital = require('to-capital-case');
+var snake = require('to-snake-case');
+var camel = require('to-camel-case');
+var nocase = require('to-no-case');
 var each = require('@ndhoule/each');
 var events = require('../lib');
 var objectKeys = require('@ndhoule/keys');
@@ -10,11 +14,11 @@ var assert = require('proclaim');
 each(function(key) {
   var regexp = events[key];
 
-  test(regexp, to.camel(key));
-  test(regexp, to.none(key));
-  test(regexp, to.snake(key));
-  test(regexp, to.sentence(key));
-  test(regexp, to.capital(key));
+  test(regexp, camel(key));
+  test(regexp, nocase(key));
+  test(regexp, snake(key));
+  test(regexp, sentence(key));
+  test(regexp, capital(key));
 
   function test(regexp, str) {
     it(str + ' == ' + regexp, function() {
